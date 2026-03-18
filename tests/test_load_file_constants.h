2f2f0eeeafdbc284a801880d5b1ce46c0c4b09b1
Luke Houston <lukehouston08@gmail.com>
2026-03-18 17:06:38 -0600

    fix formatting

--- tests/test_load_file_constants.h
+++ tests/test_load_file_constants.h
@@ -26,11 +26,8 @@
 
 constexpr int              numLines        = 11;
 constexpr std::string_view lines[numLines] = {
-    "# ignored",       "",
-    "ignore",          "!negate",
-    "charclass[0-9]",  "file.extension",
-    "path/to",         "pattern#x",
-    "dir/*",           "dir/*.ext",
+    "# ignored",       "",        "ignore",    "!negate", "charclass[0-9]",
+    "file.extension",  "path/to", "pattern#x", "dir/*",   "dir/*.ext",
     "dir/**/otherFile"};
 
 constexpr int              numPatterns           = 9;
